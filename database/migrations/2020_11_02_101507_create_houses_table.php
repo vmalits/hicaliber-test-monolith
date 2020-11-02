9a3794e39b832986fb83ad972c0c4fc6712ec93f
Vladimir Malits <vladimir.malits@gmail.com>
2020-11-02 12:54:17 +0200

    added House.php model, HouseFactory.php, generated migration, refactoring database configs

---
+++ database/migrations/2020_11_02_101507_create_houses_table.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHousesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('houses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false);
+            $table->integer('price')->default(0);
+            $table->integer('bedrooms')->default(1);
+            $table->integer('bathrooms')->default(1);
+            $table->integer('storeys')->default(1);
+            $table->integer('garages')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('houses');
+    }
+}
