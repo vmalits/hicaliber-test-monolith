0f2fcce0eb089e5cd1eff8857683e8f7b8edd25a
Vladimir Malits <vladimir.malits@gmail.com>
2020-11-02 13:48:50 +0200

    added Filterable trait to House model, added fields to fillable array

--- app/Models/House.php
+++ app/Models/House.php
@@ -1,11 +1,23 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Models;
 
+use App\QueryFilters\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class House extends Model
 {
-    use HasFactory;
+    use HasFactory, Filterable;
+
+    protected $fillable = [
+        'name',
+        'bedrooms',
+        'bathrooms',
+        'storeys',
+        'garages',
+        'price'
+    ];
 }
+
